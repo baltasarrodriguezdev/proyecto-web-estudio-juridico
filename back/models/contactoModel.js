97e2ae1942857d354335fee162e40d0eb5d8cc61
Baltasar <baltasarrodriguezt@gmail.com>
2026-02-27 11:56:44 -0300

    arreglando bug

--- back/models/contactoModel.js
+++ back/models/contactoModel.js
@@ -4,7 +4,7 @@ async function postContact(obj) {
     try {
         var query =` 
         insert into consultas (nombre,email,telefono,asunto,mensaje,estado) 
-        values ( ? , ? , ? , ? , ? , ? , ?)`
+        values ( ? , ? , ? , ? , ? , ? )`
         var rows = await pool.query(query,[obj.nombre ,obj.email, obj.telefono, obj.asunto, obj.mensaje, 'nuevo']);
         return rows;
     } catch (error) {
